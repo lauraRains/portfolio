1ceaf32e19d463131ff056bba73184cb9950f289
Laura Rains <35811828+lauraRains@users.noreply.github.com>
2018-03-10 13:09:45 -0800

    Add files via upload

---
+++ style.php
@@ -0,0 +1,20 @@
+<?php
+    
+header("Content-type: text/css; charset: UTF-8");
+
+
+$h1color = "#1C5978";
+$h2color = "#1C5978";
+
+
+?>
+
+h1 {
+color: <?php echo $h1color; ?>
+}
+
+h2 {
+color: <?php echo $h2color; ?>
+}
+
+
